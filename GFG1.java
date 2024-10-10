1504af9b5ba3544f06aee923684d8abb85baa701
Chavhare Mayuri Sudam <144438613+mayurichavhare2004@users.noreply.github.com>
2024-10-10 07:02:22 -0700

    Add files via upload
    hi

---
+++ GFG1.java
@@ -0,0 +1,13 @@
+package array;
+
+import java.io.*;
+
+class GFG1 {
+    public static void main (String[] args) {
+       
+      int [] arr=new int [4];
+      // 4 is the size of arr
+      
+      System.out.println("Array Size:"+arr.length);
+    }
+}
